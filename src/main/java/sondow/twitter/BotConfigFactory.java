6114721dc4399ca9ac79108dffe038e1844b1418
Joe Sondow <joesondow@users.noreply.github.com>
2019-07-15 04:42:14 -0700

    Switch to single env var for twitter credentials

--- src/main/java/sondow/twitter/BotConfigFactory.java
+++ src/main/java/sondow/twitter/BotConfigFactory.java
@@ -13,42 +13,24 @@ public class BotConfigFactory {
         return new BotConfig(twitterConf);
     }
 
-    /**
-     * AWS Lambda only allows underscores in environment variables, not dots, so the default ways
-     * twitter4j finds keys aren't possible. Instead, this custom code gets the configuration either
-     * from Lambda-friendly environment variables or else allows Twitter4J to look in its default
-     * locations like twitter4j.properties file at the project root, or on the classpath, or in
-     * WEB-INF.
-     * <p>
-     * The config from this method also includes keys for accessing the Twitter API, as well as
-     * some domain-specific variables.
-     *
-     * @return configuration containing AWS and Twitter authentication strings and other variables
-     */
     private Configuration configureTwitter() {
-        ConfigurationBuilder builder = new ConfigurationBuilder();
+        ConfigurationBuilder configBuilder = new ConfigurationBuilder();
 
-        // Override with a specific account if available. This mechanism allows us to provide
-        // multiple key sets in the AWS Lambda configuration, and switch which Twitter account
-        // to target by retyping just the target Twitter account name in the configuration.
-        String account = Environment.require("twitter_account");
-        builder.setUser(account);
+        String credentialsCsv = Environment.require("twitter_credentials");
 
-        String consumKeyEnvKey = "twitter4j_oauth_consumerKey";
-        String consSecEnvKey = "twitter4j_oauth_consumerSecret";
-        String accTokEnvKey = "twitter4j_oauth_accessToken";
-        String atsEnvKey = "twitter4j_oauth_accessTokenSecret";
+        String[] tokens = credentialsCsv.split(",");
+        String screenName = tokens[0];
+        String consumerKey = tokens[1];
+        String consumerSecret = tokens[2];
+        String accessToken = tokens[3];
+        String accessTokenSecret = tokens[4];
 
-        String consumerKey = Environment.either(account + "_" + consumKeyEnvKey, consumKeyEnvKey);
-        String consumerSecret = Environment.either(account + "_" + consSecEnvKey, consSecEnvKey);
-        String accessToken = Environment.either(account + "_" + accTokEnvKey, accTokEnvKey);
-        String accessTokenSecret = Environment.either(account + "_" + atsEnvKey, atsEnvKey);
+        configBuilder.setUser(screenName);
+        configBuilder.setOAuthConsumerKey(consumerKey);
+        configBuilder.setOAuthConsumerSecret(consumerSecret);
+        configBuilder.setOAuthAccessToken(accessToken);
+        configBuilder.setOAuthAccessTokenSecret(accessTokenSecret);
 
-        builder.setOAuthConsumerKey(consumerKey);
-        builder.setOAuthConsumerSecret(consumerSecret);
-        builder.setOAuthAccessToken(accessToken);
-        builder.setOAuthAccessTokenSecret(accessTokenSecret);
-
-        return builder.setTrimUserEnabled(true).build();
+        return configBuilder.setTrimUserEnabled(false).build();
     }
 }
--- src/test/groovy/sondow/twitter/BotConfigFactorySpec.groovy
+++ src/test/groovy/sondow/twitter/BotConfigFactorySpec.groovy
@@ -12,12 +12,9 @@ class BotConfigFactorySpec extends Specification {
 
     def "configure should populate a configuration with environment variables"() {
         setup:
-        String filler = Environment.SPACE_FILLER
-        envVars.set("twitter_account", "cartoons")
-        envVars.set("cartoons_twitter4j_oauth_consumerKey", "${filler}mickeymouse")
-        envVars.set("cartoons_twitter4j_oauth_consumerSecret", "${filler}georgejetson")
-        envVars.set("cartoons_twitter4j_oauth_accessToken", "${filler}fredflintstone")
-        envVars.set("cartoons_twitter4j_oauth_accessTokenSecret", "${filler}bugsbunny")
+        String f = Environment.SPACE_FILLER
+        String val = "${f}${f}cartoons,mickeymouse,georgejetson,fredflintstone,bugsbunny${f}${f}"
+        envVars.set("twitter_credentials", val)
 
         when:
         BotConfig tetraConfig = new BotConfigFactory().configure()
@@ -29,31 +26,6 @@ class BotConfigFactorySpec extends Specification {
             OAuthConsumerSecret == 'georgejetson'
             OAuthAccessToken == 'fredflintstone'
             OAuthAccessTokenSecret == 'bugsbunny'
-        }
-    }
-
-    def "configure should populate a configuration with account specific env vars"() {
-        setup:
-        String filler = Environment.SPACE_FILLER
-        envVars.set("twitter_account", "picardtips")
-        envVars.set("picardtips_twitter4j_oauth_consumerKey", "${filler}flute")
-        envVars.set("picardtips_twitter4j_oauth_consumerSecret", "${filler}makeitso")
-        envVars.set("picardtips_twitter4j_oauth_accessToken", "${filler}therearefourlight")
-        envVars.set("picardtips_twitter4j_oauth_accessTokenSecret", "${filler}engage")
-        envVars.set("rikergoogling_twitter4j_oauth_consumerKey", "${filler}whatthehell")
-        envVars.set("rikergoogling_twitter4j_oauth_consumerSecret", "${filler}minuet")
-        envVars.set("rikergoogling_twitter4j_oauth_accessToken", "${filler}jazz")
-        envVars.set("rikergoogling_twitter4j_oauth_accessTokenSecret", "${filler}beard")
-
-        when:
-        Configuration config = new BotConfigFactory().configure().getTwitterConfig()
-
-        then:
-        with(config) {
-            OAuthConsumerKey == 'flute'
-            OAuthConsumerSecret == 'makeitso'
-            OAuthAccessToken == 'therearefourlight'
-            OAuthAccessTokenSecret == 'engage'
         }
     }
 }
