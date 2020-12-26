cd167f2bf4169ff4dd6aa87416b36c75732866be
Joe Sondow <joesondow@users.noreply.github.com>
2020-12-26 15:36:30 -0800

    TargetChooser stub class

---
+++ src/main/java/sondow/twitter/TargetChooser.java
@@ -0,0 +1,10 @@
+package sondow.twitter;
+
+public class TargetChooser {
+
+    public String chooseTarget() {
+        // Based on current day, choose target account name.
+
+        return "EmojiAquarium";
+    }
+}
